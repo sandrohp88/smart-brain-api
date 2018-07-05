b1c3e2553afb3acd5406ca2babc0331191627d62
Sandro Herrera Pallares <sandrohp88@gmail.com>
2018-07-05 12:27:31 -0700

    fix DB connection

--- server.js
+++ server.js
@@ -20,13 +20,8 @@ const db = knex({
   client: "pg",
 
   connection: {
-    host: "127.0.0.1",
-
-    user: "aneagoie",
-
-    password: "",
-
-    database: "smart-brain"
+    connectionString: process.env.DATA_BASE_URL,
+    ssl: true,
   }
 });
 
